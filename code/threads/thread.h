0c6a4fa6b5598e555889e468d27f3f9bb1dc92aa
derdaelj <jeremy.derdaele@gmail.com>
2015-01-12 02:20:02 +0100

    start work on process

--- code/threads/thread.h
+++ code/threads/thread.h
@@ -1,9 +1,9 @@
-// thread.h 
+// thread.h
 //      Data structures for managing threads.  A thread represents
 //      sequential execution of code within a program.
 //      So the state of a thread includes the program counter,
 //      the processor registers, and the execution stack.
-//      
+//
 //      Note that because we allocate a fixed size stack for each
 //      thread, it is possible to overflow the stack -- for instance,
 //      by recursing to too deep a level.  The most common reason
@@ -17,12 +17,12 @@
 //              void foo() { int *buf = new int[1000]; ...}
 //
 //
-//      Bad things happen if you overflow the stack, and in the worst 
+//      Bad things happen if you overflow the stack, and in the worst
 //      case, the problem may not be caught explicitly.  Instead,
 //      the only symptom may be bizarre segmentation faults.  (Of course,
 //      other problems can cause seg faults, so that isn't a sure sign
 //      that your thread stacks are too small.)
-//      
+//
 //      One thing to try if you find yourself with seg faults is to
 //      increase the size of thread stack -- ThreadStackSize.
 //
@@ -31,7 +31,7 @@
 //      Only then can we do the fork: "t->fork(f, arg)".
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #ifndef THREAD_H
@@ -52,7 +52,7 @@
 
 #endif // USER_PROGRAM
 
-// CPU register state to be saved on context switch.  
+// CPU register state to be saved on context switch.
 // The SPARC and MIPS only need 10 registers, but the Snake needs 18.
 // For simplicity, this is just the max over all architectures.
 #define MachineStateSize 18
@@ -77,7 +77,7 @@ extern void ThreadPrint (int arg);
 //     an execution stack for activation records ("stackTop" and "stack")
 //     space to save CPU registers while not running ("machineState")
 //     a "status" (running/ready/blocked)
-//    
+//
 //  Some threads also belong to a user address space; threads
 //  that only run in the kernel have a NULL address space.
 
@@ -90,22 +90,22 @@ class Thread
     int machineState[MachineStateSize];	// all registers except for stackTop
 
   public:
-      Thread (const char *debugName);	// initialize a Thread 
+      Thread (const char *debugName);	// initialize a Thread
      ~Thread ();		// deallocate a Thread
     // NOTE -- thread being deleted
-    // must not be running when delete 
+    // must not be running when delete
     // is called
 
     // basic thread operations
 
     void Fork (VoidFunctionPtr func, int arg);	// Make thread run (*func)(arg)
-    void Yield ();		// Relinquish the CPU if any 
+    void Yield ();		// Relinquish the CPU if any
     // other thread is runnable
-    void Sleep ();		// Put the thread to sleep and 
+    void Sleep ();		// Put the thread to sleep and
     // relinquish the processor
     void Finish ();		// The thread is done executing
 
-    void CheckOverflow ();	// Check if thread has 
+    void CheckOverflow ();	// Check if thread has
     // overflowed its stack
     void setStatus (ThreadStatus st)
     {
@@ -123,7 +123,7 @@ class Thread
   private:
     // some of the private data for this class is listed above
 
-    int *stack;			// Bottom of the stack 
+    int *stack;			// Bottom of the stack
     // NULL if this is the main thread
     // (If NULL, don't deallocate stack)
     ThreadStatus status;	// ready, running or blocked
@@ -134,8 +134,8 @@ class Thread
     // Used internally by Fork()
 
 #ifdef USER_PROGRAM
-// A thread running a user program actually has *two* sets of CPU registers -- 
-// one for its state while executing user code, one for its state 
+// A thread running a user program actually has *two* sets of CPU registers --
+// one for its state while executing user code, one for its state
 // while executing kernel code.
 
     int userRegisters[NumTotalRegs];	// user-level CPU register state
@@ -147,8 +147,14 @@ class Thread
     AddrSpace *space;		// User code this thread is running.
 
 #ifdef CHANGED
+
+	// Process
+	int pid;
+	int ppid;
+	//
+
+	// User threads
     int userarg;
-    
     int *maxtid; // count of threads in the process
     int tid; // id of this thread
     Semaphore *joinsem; // semaphore used for thread joining
@@ -157,6 +163,8 @@ class Thread
     int *threadcount;
 
     std::queue<int> *availabletids;
+	// -----------
+
 #endif // CHANGED
 #endif
 };
@@ -165,7 +173,7 @@ class Thread
 
 extern "C"
 {
-// First frame on thread execution stack; 
+// First frame on thread execution stack;
 //      enable interrupts
 //      call "func"
 //      (when func returns, if ever) call ThreadFinish()
---
+++ code/userprog/process_manager.cc
@@ -0,0 +1,111 @@
+#include "process_ma
+#include "system.h"
+#include "addrspace.h"
+#include <list>
+#include <queue>
+
+ProcessManager::ProcessManager()
+{
+
+}
+
+ProcessManager::~ProcessManager()
+{
+
+}
+
+void ProcessManager::initProcess(Thread *th, int pid, int ppid)
+{
+	th->maxtid = new int;
+	*(th->maxtid) = 1;
+
+	th->tid = 0;
+
+	th->userThreads = new list<Thread*>;
+	th->userThreads->push_back(th);
+
+	th->avaiabletids = new queue<int>;
+
+	th->threadcount = new int;
+
+	*(th->threadcount) = 1;
+
+}
+
+void ProcessManager::Init(const char *filename)
+{
+	OpenFile *executable = fileSystem->Open(filename);
+	AddrSpace *space;
+
+	if (executable == NULL)
+	{
+		ASSERT(FALSE && "Can't open init file, aborting\n");
+	}
+
+	space = new AddrSpace(executable);
+	currentThread->space = space;
+
+	initProcess(currentThread);
+
+	delete executable;
+
+	space->InitRegisters();
+	space->RestoreState();
+
+	machine->Run ();
+	ASSERT (FALSE); 			// Shall never be reached
+}
+
+static void DoFork(int arg)
+{
+	// TODO:
+	//
+	// WE NEED TO RETURN THE VALUE 0
+	// AND ADVANCE PC
+	//
+	// SAME FOR LR
+	// SP SHOULD BE RESTORED
+	machine->Run();
+}
+
+int ProcessManager::Fork()
+{
+	AddrSpace *space = new AddrSpace(currentThread->space);
+	Thread *newThread = new Thread("user process");
+	int pid; // ALLOCATE PID
+
+	newThread->space = space;
+
+	initProcess(newThread);
+
+	newThread->Fork(DoFork, 0);
+}
+
+int ProcessManager::Exec(const char *filename)
+{
+	OpenFile *executable = fileSystem->Open(filename);
+	AddrSpace *space;
+
+	if (executable == NULL)
+	{
+		ASSERT(FALSE && "Can't open init file, aborting\n");
+	}
+
+	space = new AddrSpace(executable);
+
+	delete currentThread->space;
+
+	currentThread->space;
+
+	space->InitRegisters();
+}
+
+int ProcessManager::Exit(int exit_code)
+{
+
+}
+
+int ProcessManager::WaitPid(int pid)
+{
+
+}
