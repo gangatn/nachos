06c4dea7245df96cc0171ed52b9cd6ba5e12b5e4
ngangat <ngangat.public@gmail.com>
2015-01-12 10:57:58 +0100

    Step 4, Action I.1 : little test program, which write some chars using 2 threads

---
+++ code/test/userpages0.c
@@ -0,0 +1,18 @@
+#include "syscall.h"
+
+void writeChars(void * arg) {
+  int n = (int) arg;
+  char a = n % 2 == 0  ? 'a' : 'A';
+  char i;
+  for (i = a ; i < a + 26; ++i) {
+    PutChar(i);
+  }
+}
+
+int main() {
+  int t1 = UserThreadCreate(writeChars, (void*)1);
+  int t2 = UserThreadCreate(writeChars, (void*)2);
+  UserThreadJoin(t1);
+  UserThreadJoin(t2);
+  return 0;
+}
