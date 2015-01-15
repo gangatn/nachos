a7525bad0d368f914c054a03528cfe68b97ba5ff
derdaelj <jeremy.derdaele@gmail.com>
2015-01-15 17:57:30 +0100

    frameprovider: use ref counter instead of bitmap
    The bitmap approach of the frameprovider was good enough
    for start, but there is some case when we want multiple process
    can use the same physical page.
    
    For copy on write or mmap for example.

--- code/userprog/frameprovider.cc
+++ code/userprog/frameprovider.cc
@@ -4,39 +4,92 @@
 
 
 FrameProvider::FrameProvider()
-	: bitmap(NumPhysPages)
 {
-
+	frame_count = NumPhysPages;
+	frame_ref = new short[frame_count];
+	memset(frame_ref, 0, sizeof(*frame_ref) * frame_count);
 }
 
 FrameProvider::~FrameProvider()
 {
-
+	delete [] frame_ref;
 }
 
 
+int FrameProvider::findFreeFrame()
+{
+	int i;
+	int found = -1;
+
+	for(i = 0; i < frame_count; i++)
+	{
+		if(frame_ref[i] == 0)
+		{
+			found = i;
+			break;
+		}
+	}
+
+	return found;
+}
+
+void FrameProvider::UseFrame(int frame)
+{
+	// We use an assert, if an invalid frame is provided
+	// this is most likely a programming error
+	// (but not always, memory corruption can eventually
+	//  trigger this kind of issue)
+	ASSERT(frame >= 0 && frame < frame_count);
+	frame_ref[frame]++;
+}
+
 int FrameProvider::GetEmptyFrame()
 {
-	int frame = bitmap.Find();
-	bzero(&machine->mainMemory[frame*PageSize], PageSize);
+	int frame = findFreeFrame();
+
+	if (frame >= 0)
+	{
+		UseFrame(frame);
+		/* We use memset, bzero is deprecated */
+		memset(&machine->mainMemory[frame*PageSize], 0, PageSize);
+	}
 	return frame;
 }
 
 int FrameProvider::GetCopiedFrame(int copied_frame)
 {
-	int frame = bitmap.Find();
-	memcpy(&machine->mainMemory[frame*PageSize], &machine->mainMemory[copied_frame*PageSize], PageSize);
+	int frame = findFreeFrame();
+
+	if (frame >= 0)
+	{
+		UseFrame(frame);
+		memcpy(&machine->mainMemory[frame*PageSize], &machine->mainMemory[copied_frame*PageSize], PageSize);
+	}
 	return frame;
 }
 
 
 void FrameProvider::ReleaseFrame(int frame)
 {
-	bitmap.Clear(frame);
+	ASSERT(frame >= 0 && frame < frame_count);
+	ASSERT(frame_ref[frame] > 0);
+
+	frame_ref[frame]--;
 }
 
 
 int FrameProvider::NumAvailFrame()
 {
-	return bitmap.NumClear();
+	int i;
+	int avail = 0;
+
+	// TODO: This implementation is currently
+	// inefficient, this can be improved
+	// using size counters
+	for (i = 0; i < frame_count; i++)
+	{
+		if(frame_ref[i ] == 0) avail++;
+	}
+
+	return avail;
 }
--- code/userprog/frameprovider.h
+++ code/userprog/frameprovider.h
@@ -3,10 +3,14 @@
 // Physical frame provider, used by virtual memory mechanism
 // i.e. paging to associate physical memory pages to
 // a virtual page
+//
+// Our implementation uses a reference counter instead of a bitmap
+// this permit us to allow multiple user for one given frame
+//
 #ifndef FRAMEPROVIDER_H
 #define FRAMEPROVIDER_H
 
-#include "bitmap.h"
+
 
 class FrameProvider
 {
@@ -14,16 +18,37 @@ public:
 	FrameProvider();
 	~FrameProvider();
 
+	// GetEmptyFrame:
+	//
 	// Return a valid physical empty frame
 	// this frame shall not be returned
 	// again by multiple calls until released
 	//
 	// All bytes of the frame equals 0
+	//
+	// This also mark the returned frame as used
+	//
+	// Return -1 if no frame is available
 	int GetEmptyFrame();
 
+	// GetCopiedFrame:
+	//
 	// Return a valid physical frame that is
 	// a copy of the given frame
+	//
+	// This also mark the returned frame as used
+	//
+	// Return -1 if no frame is available
 	int GetCopiedFrame(int frame);
+
+	// UseFrame:
+	//
+	// Indicated the frame provider that there
+	// is a new user to the given thread
+	// This is *OBLIGATORY* to call this function
+	// if you use a frame that you did not get using
+	// the GetEmptyFrame or the GetCopiedFrame functions
+	void UseFrame(int frame);
 
 	// Release the given frame
 	// This frame shall not be used again
@@ -33,7 +58,10 @@ public:
 	// Returns the number of available frames
 	int NumAvailFrame();
 private:
-	BitMap bitmap;
+	short *frame_ref;
+	int frame_count;
+
+	int findFreeFrame();
 };
 
 
