3a7cd88c86c31c58c1f9925c5394ed999781bde6
dmnj <jpdgz+github@hspaz.org>
2015-01-16 13:04:06 +0100

    More tests with Fork, WaitPid, getpid, getppid

--- code/test/testforkandwait.c
+++ code/test/testforkandwait.c
@@ -1,5 +1,14 @@
 #include "syscall.h"
 
+void printpid()
+{
+  PutString("pid=");
+  PutInt(getpid());
+  PutString(", ppid=");
+  PutInt(getppid());
+  PutString("\n");
+}
+
 int main(void)
 {
   int pid;
@@ -8,15 +17,40 @@ int main(void)
   pid = Fork();
 
   if (pid != 0) {
-    PutString("I am the father, waiting for the son\n");
+    PutString("I am the father, fork and wait for the child | ");
+    printpid();
     WaitPid(pid);
-    PutString("Son terminated\n");
+    PutString("Child terminated\n");
   } else {
-    PutString("I am the son\n");
+    PutString("I am the child, fork and wait for the subchild | ");
+    printpid();
     for (i = 0 ; i < 100000 ; ++i);
-    PutString("Son exit now\n");
+    pid = Fork();
+    if (pid == 0) {
+      PutString("I am the subchild | ");
+      printpid();
+      PutString("Subchild exit\n");
+      Exit(0);
+    }
+    WaitPid(pid);
+    PutString("Subchild terminated\n");
     Exit(0);
   }
+
+  pid = Fork();
+
+  if (pid == 0) {
+    printpid();
+    pid = Fork();
+    if (pid != 0) {
+      Exit(0);
+    } else {
+      for (i = 0 ; i < 100000 ; ++i);
+      printpid();
+      Exit(0);
+    }
+  } else
+    WaitPid(pid);
   
   return 0;
 }
